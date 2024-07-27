8199f06b14bdd6470549e46429815ff52b4026d5
Pedro Martinez <pittermartinez9@gmail.com>
2024-07-27 00:19:43 -0700

    detalle pequeño

--- paginas/index.php
+++ paginas/index.php
@@ -6,21 +6,25 @@
             <div class="item">
                 <a href="fiesta.php">
                 <img src="../Imagenes/fiesta.jpg" class="fiesta" alt="Artículos de fiesta">
+                </a>
                 <p>Artículos de fiesta</p>
             </div>
             <div class="item">
                 <a href="libretas.php">
                 <img src="../Imagenes/libretas.jpg" class="libretas" alt="Libretas">
+                </a>
                 <p>Libretas</p>
             </div>
             <div class="item">
                 <a href="papeleria.php">
                 <img src="../Imagenes/papeleria.jpg" class="papeleria" alt="Papelería">
+                </a>
                 <p>Papelería</p>
             </div>
             <div class="item">
                 <a href="utiles.php">
                 <img src="../Imagenes/utiles.jpg" class="utiles" alt="Útiles Escolares">
+                </a>
                 <p>Útiles Escolares</p>
             </div>
         </div>
